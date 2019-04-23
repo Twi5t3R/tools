f72399a4aa25edd05f5fbad88a3916e1cda1843e
Twi5t3R <49911394+Twi5t3R@users.noreply.github.com>
2019-04-23 20:39:54 +0700

    Add files via upload

---
+++ tools.sh
@@ -0,0 +1,211 @@
+#!/system/xbin/bash
+clear
+
+echo "\033[36;1mMohon tunggu..."
+sleep 3
+clear
+echo "\033[33;1mMemproses Data..."
+sleep 3
+clear
+echo "\033[34;1mAkan selesai dalam!!"
+sleep 3
+clear
+echo "\033[31;1m (4)"
+sleep 3
+clear
+echo "\033[32;1m (3)"
+sleep 3
+clear
+echo "\033[33;1m (2)"
+sleep 3
+clear
+echo "\033[36;1m (1)"
+sleep 3
+clear
+echo "\033[31;1m GAGAL!! Mencoba ulang..."
+sleep 5
+clear
+echo "\033[35;1m Selesai!!"
+sleep 3
+clear
+
+
+echo "\033[36;1m             ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "\033[33;1m             ■  Selamat Datang Di tools Saya!!!  ■"
+echo "\033[32;1m             ■        Author : •Twist3R•         ■"
+echo "\033[31;1m             ■        contact : 081335497168     ■"
+echo "\033[34;1m             ■        Spesial Thanks :           ■"
+echo "\033[33;1m             ■            •Deniganz              ■"
+echo "\033[35;1m             ■            •3M0N27                ■"
+echo "\033[34;1m             ■        Spesial Group  :           ■"
+echo "\033[31;1m             ■     •Tutorial Termux Indonesia    ■"
+echo "\033[37;1m             ■     •LSQ#Crew(Lendir_SQuad)       ■"
+echo "\033[36;1m             ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 3
+echo "\033[32;1m * Tools ini buka 24 jam nonstop"
+sleep 1
+echo "\033[32;1m * Kami menyediakan banyak menu silahkan pilih"
+echo "\033[33;1m▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo "\033[33;1m""\033[35;1m(1)""\033[36;1mMembuat Script Deface"
+sleep 1
+echo "\033[33;1m""\033[35;1m(2)""\033[36;1mHack FB               "
+sleep 1
+echo "\033[33;1m""\033[35;1m(3)""\033[36;1mHack CCTV             "
+sleep 1
+echo "\033[33;1m""\033[35;1m(4)""\033[36;1mSadap Whatsapp        "
+sleep 1
+echo "\033[33;1m""\033[35;1m(5)""\033[36;1mAnimasi kereta        "
+sleep 1
+echo "\033[33;1m""\033[35;1m(6)""\033[36;1mMembuat Virus         "
+sleep 1
+echo "\033[33;1m""\033[35;1m(7)""\033[36;1mKalkulator            "
+sleep 1
+echo "\033[33;1m""\033[35;1m(8)""\033[36;1mPulsa Gratis"
+sleep 1
+echo "\033[33;1m""\033[35;1m(9)""\033[36;1mDDOS"
+sleep 1
+echo "\033[33;1m""\033[31;1m(0) Keluar"
+echo "\033[33;1m▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄"
+sleep 1
+echo "\033[33;1mSilahkan pilih NOMOR,jangan malu!!!"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/Ubaii/script-deface-creator
+mv script-deface-creator $HOME
+cd $HOME/script-deface-creator
+python2 create.py
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/Mrdeniganz/MBF
+mv MBF $HOME
+cd $HOME/MBF
+python2 MBF.py
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update
+apt upgrade
+pkg install git
+git clone https://github.com/kancotdiq/ipcs
+mv ipcs $HOME
+cd $HOME/ipcs
+python2 scan.py
+
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+git clone https://github.com/AndriGanz/whatshack
+mv whatshack $HOME
+cd $HOME/whatshack
+sh whatshack.sh
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+pkg install sl
+sl
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update && apt upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/rickyricko302/virus-py
+mv virus-py $HOME
+cd $HOME/virus-py
+python2 virus-gen.py
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update
+apt upgrade
+apt install git
+apt install python2
+git clone https://github.com/3M0N27/kalkulator
+mv kalkulator $HOME
+cd $HOME/kalkulator
+python2 kalkulator.py
+fi
+
+if
+[ $mrrm = 8 ] || [ $mrrm = 8 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+echo "\033[36;1m Memproses Data..."
+sleep 4
+echo "\033[35;1m Berhasil!!"
+sleep 5
+echo "\033[34;1m Hari gini masih ngarepin gratisan??,Kampungan!!"
+sleep 3
+echo "\033[33;1m KYETIPUY!!!"
+sleep 2
+fi
+
+if
+[ $mrrm = 9 ] || [ $mrrm = 9 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+apt update && apt upgrade
+pkg install git
+pkg install python2
+git clone https://github.com/4L13199/LITEDDOS
+mv LITEDDOS $HOME
+cd $HOME/LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+clear
+toilet -f slant "•Twist3R•"
+sleep 2
+echo "\033[36;1mTerima kasih sudah berkunjung!!"
+sleep 1
+echo "\033[32;1mSampai jumpa lagi!"
+sleep 1
+fi
